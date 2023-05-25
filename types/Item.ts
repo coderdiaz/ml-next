fd33ceb6e66f5f9cc24bbf3eab8729a115a86a9b
Javier Diaz <coderdiaz@gmail.com>
2023-05-25 14:45:25 -0600

    feat: added new types

---
+++ types/Item.ts
@@ -0,0 +1,31 @@
+type Author = {
+  name: string;
+  lastname: string;
+}
+
+type Price = {
+  currency: string;
+  amount: number;
+  decimals: string;
+}
+
+type Item = {
+  id: string;
+  title: string;
+  price: Price;
+  picture: string;
+  condition: string;
+  free_shipping: boolean;
+  description?: string;
+}
+
+export type MLItemsPayload = {
+  author: Author;
+  categories: string[];
+  items: Item[];
+}
+
+export type MLItemPayload = {
+  author: Author;
+  item: Item;
+}
