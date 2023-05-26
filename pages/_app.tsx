71b706374e98fc60741edcaa3c4443ec7ae79cc8
Javier Diaz <coderdiaz@gmail.com>
2023-05-25 20:39:07 -0600

    fix: customized app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app';
+import Header from '@/components/Header';
 import 'tailwindcss/tailwind.css';
  
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
