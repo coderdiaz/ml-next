9cf59c95c75f481518e324084bcbbc4b20cb49fd
Javier Diaz <coderdiaz@gmail.com>
2023-05-25 20:36:57 -0600

    feat: added colors to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     './pages/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      meli: {
+        primary: '#ffe600',
+        secondary: '#3483fa',
+      },
+      gray: {
+        500: '#333333',
+        400: '#666666',
+        300: '#999999',
+        200: '#eeeeee',
+      },
+      white: '#ffffff',
+      black: '#000000',
+    },
   },
   plugins: [],
 }
